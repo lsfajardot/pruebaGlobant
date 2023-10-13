50e98c40d0b86b367bb688f94d6192f093791ee8
lsfajardot <luigifajardotoloza@gmail.com>
2023-10-12 20:19:13 -0500

    Se añade segunda metrica

--- app.py
+++ app.py
@@ -98,6 +98,53 @@ def get_metric1():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+@app.route('/metric2', methods=['GET'])
+def get_metric2():
+    try:
+        # Conectar a la base de datos SQLite (asegúrate de tenerla en el directorio de tu proyecto o proporciona una ruta)
+        connection = sqlite3.connect('company.db')
+        cursor = connection.cursor()
+
+        # Realizar la consulta SQL
+        consulta = """
+                select e.id, e.department, e.conteo as hired from (select b.id as id, b.department as department, d.media as media , count(a.id) as conteo 
+                from (select año, avg(conteo) as media from (SELECT b.id, b.department, strftime('%Y', a.hired_date) AS año, count(a.id) as conteo
+                FROM employees A 
+                INNER JOIN departments B ON A.department_id = B.id
+                INNER JOIN jobs C ON A.job_id = C.id
+                WHERE strftime('%Y', a.hired_date) = '2021'
+                group by b.id, b.department, strftime('%Y', a.hired_date)) as c
+                group by año) as d
+                INNER join employees a on strftime('%Y', a.hired_date)=d.año
+                inner JOIN departments b on b.id=a.department_id
+                group by b.id, b.department, d.media) as e
+                where e.conteo>e.media
+                order by e.conteo desc 
+            """
+
+        cursor.execute(consulta)
+        resultados = cursor.fetchall()
+
+        # Cerrar la conexión a la base de datos
+        connection.close()
+
+        # Formatear los resultados y devolverlos como JSON
+        respuesta = []
+        for fila in resultados:
+            id, department, hired = fila
+            respuesta.append({
+                'Id': id,
+                'Department': department,
+                'Hired': hired
+            })
+
+        return jsonify(respuesta)
+        #return render_template('metrica1.html', resultados=respuesta)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
